6a87bc93fa3042c068acdd44f4da0584a4485a42
ronak-48 <shahronak1092@gmail.com>
2025-12-06 18:49:48 +0000

    conversion o datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,37 @@
+// // let score = 33
+// // let score = null
+// // let score = undefined
+// // let score = true
+
+// let score = "33"
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// // conversion of string to number
+// let valueInNumber = Number(score)
+// console.log(valueInNumber);
+
+// let score1 = "33abc"
+// let valueInNumber1 = Number(score1)
+// console.log(valueInNumber1)
+
+/*
+"33" => 33
+"33abc" =>NaN
+true => 1 ; false =>0
+*/
+
+let isloggedIn = 1;
+let booleanIsLoggedIn = Boolean(isloggedIn);
+console.log(booleanIsLoggedIn);
+
+// 1 ==> true;   0 ==> false
+// "" ==>false
+// "ram"  ==>true
+
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
+
