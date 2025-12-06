3ada38eae1e5773ae3181ce0583ddd9551cd1dc2
ronak-48 <shahronak1092@gmail.com>
2025-12-06 17:17:18 +0000

    data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,63 @@
+"use strict"; // treat all JS code as newer version
+
+//alert(3+3) // wea re using nodejs, not browser
+
+console.log(3 + 3);
+
+console.log("riteshh");
+
+// documentation :--
+
+// https://developer.mozilla.org/en-US/docs/Web/API/Window/alert
+
+// https://tc39.es/ecma262/
+
+// ---------------------------------------------------------------------
+
+let name = "ronak";
+let age = 18;
+let isLoggedIn = false;
+let state;
+
+//          PRIMITIVE DATA-TYPES
+
+
+// number => 2 to power 53
+// bigint
+// string => " "
+// boolean => true/false
+// null => standalone value
+    // ek representation hai empty value ka ,its a special type 
+    // eg = server mei kuch dikaat aayi toh , HUME TEMP 0 NAHI chaiye, i.e  we'll prefer "null" 
+    //  means empty temp aaya hi nahi
+
+// undefined => 
+    // means ki value assign/define nahi ki/huie hain
+
+// symbol => for uniqueness we use them
+
+
+
+//               NON-PRIMITIVE DATA-TYPES
+
+
+// OBJECT
+
+
+// -----------------------------------------
+console.log(typeof "hitesh");
+console.log(typeof age);
+console.log(typeof isLoggedIn);
+console.log(typeof state);
+
+
+console.log(typeof null);
+// typeof null is ==> OBJECT
+
+console.log(typeof undefined);
+// typeof undefined is   ==> UNDEFINED
+
+
+
+
+
