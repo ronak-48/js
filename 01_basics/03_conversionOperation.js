91f01e273cc28503f1e864f3633d06071c4445ef
ronak-48 <shahronak1092@gmail.com>
2025-12-07 17:14:32 +0000

    conversions

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -23,6 +23,8 @@ true => 1 ; false =>0
 */
 
 // let isloggedIn = 1;
+// let isloggedIn = "" // false
+// let isloggedIn = "ram" // true
 // let booleanIsLoggedIn = Boolean(isloggedIn);
 // console.log(booleanIsLoggedIn);
 
@@ -31,16 +33,14 @@ true => 1 ; false =>0
 // "" ==>false
 // "ram"  ==>true
 
-
 // let someNumber = 33
 // let stringNumber = String(someNumber)
 // console.log(stringNumber)
 // console.log(typeof stringNumber)
 
-
 // *************************    OPERATIONS   *********************************
-let value = 3
-let negValue = -value
+let value = 3;
+let negValue = -value;
 // console.log(negValue);
 
 // console.log(2+2)
@@ -50,41 +50,44 @@ let negValue = -value
 // console.log(2/3)
 // console.log(2%3)
 
-//              STRING ADDITION
+//----------------STRING ADDITION-----------------------
 // let str1 = "hello"
 // let str2 = "shah"
 
 // let str3 = str1 + str2
 // console.log(str3)
 
-console.log("1" + 2); // 12
-console.log(1  + "2"); // 12
+// console.log("1" + 2); // 12
+// console.log(1  + "2"); // 12
 
-console.log("1" + "2");  // 12
+// console.log("1" + "2");  // 12
 
-console.log("1" + 2 + 2); // 122 
+// console.log("1" + 2 + 2); // 122
 
-console.log(1 + 2 + "2"); //  3 2
+// console.log(1 + 2 + "2"); //  3 2
 
 // STRING FIRST TO SABHI KA STRING MEI CONVERSION HO JAYEGA
 //  SRING LAST MEI HAI TO TOH PHELE NUMBERS KI CALCULATION HO GI FIR STRING AS ITS SIDE MEI ADD HOGI
 
-// only ggod for exams, not in real life 
-console.log(3+ 4 * 5 % 3);
+// only ggod for exams, not in real life
+// console.log(3+ 4 * 5 % 3);
 
+// ----------------------------
+// console.log(true);
+// console.log(+true);
 
+// console.log(true+); // error
 
+// console.log(+""); // 0
+let num1, num2, num3;
+num1 = num2 = num3 = 2 + 2;
+// console.log(num1,num2,num3);
 
+let gameCounter = 100
+// gameCounter++;
+++ gameCounter
+console.log(gameCounter);
 
-
-
-
-
-
-
-
-
-
-
-
+// --------------------- prefix-post fix reference to study ----------------
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
 
