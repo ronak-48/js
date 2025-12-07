26d7bdd585ad0590e6dcd32fbd71713cee380b76
ronak-48 <shahronak1092@gmail.com>
2025-12-07 06:06:26 +0000

    conversion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -15,23 +15,76 @@
 // let valueInNumber1 = Number(score1)
 // console.log(valueInNumber1)
 
+// ------------------------------------------
 /*
 "33" => 33
 "33abc" =>NaN
 true => 1 ; false =>0
 */
 
-let isloggedIn = 1;
-let booleanIsLoggedIn = Boolean(isloggedIn);
-console.log(booleanIsLoggedIn);
+// let isloggedIn = 1;
+// let booleanIsLoggedIn = Boolean(isloggedIn);
+// console.log(booleanIsLoggedIn);
 
+// --------------------------------------
 // 1 ==> true;   0 ==> false
 // "" ==>false
 // "ram"  ==>true
 
 
-let someNumber = 33
-let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// let someNumber = 33
+// let stringNumber = String(someNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+
+// *************************    OPERATIONS   *********************************
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3) // ** acts as power 2^3 =8
+// console.log(2/3)
+// console.log(2%3)
+
+//              STRING ADDITION
+// let str1 = "hello"
+// let str2 = "shah"
+
+// let str3 = str1 + str2
+// console.log(str3)
+
+console.log("1" + 2); // 12
+console.log(1  + "2"); // 12
+
+console.log("1" + "2");  // 12
+
+console.log("1" + 2 + 2); // 122 
+
+console.log(1 + 2 + "2"); //  3 2
+
+// STRING FIRST TO SABHI KA STRING MEI CONVERSION HO JAYEGA
+//  SRING LAST MEI HAI TO TOH PHELE NUMBERS KI CALCULATION HO GI FIR STRING AS ITS SIDE MEI ADD HOGI
+
+// only ggod for exams, not in real life 
+console.log(3+ 4 * 5 % 3);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
