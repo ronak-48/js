6f1c9214f8124ecb97a404a411cfe76e24fc352d
ronak-48 <shahronak1092@gmail.com>
2025-12-08 18:26:49 +0000

    ARRAY

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,4 @@
+// ARRAY
+// Array Object, enables storing a collection of multiple items under a single variable name , and has members for performing common array operations.
+
+const myArr = [0, 1, 3, 4, 5];
